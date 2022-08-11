b36fb900be15a7dc5d11e55dd4a8a03053f48151
Taylor Guenthner <wtguenthner@gmail.com>
2022-08-11 00:21:31 -0400

    Okay

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,31 +11,31 @@ const { User } = require("../../models");
 // });
 
 // CREATE new user
-router.post("/", async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      username: req.body.username,
-    });
-    if (dbUserData) {
-      res.status(400).json({ message: "Username already exists" });
-      return;
-    } else {
-      await User.create({
-        username: req.body.username,
-        password: req.body.password,
-        character_name: req.body.character_name,
-        class_id: req.body.class_id,
-      }).then(
-        req.session.save(() => {
-          req.session.loggedIn = true;
-        })
-      );
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     const dbUserData = await User.findOne({
+//       username: req.body.username,
+//     });
+//     if (dbUserData) {
+//       res.status(400).json({ message: "Username already exists" });
+//       return;
+//     } else {
+//       await User.create({
+//         username: req.body.username,
+//         password: req.body.password,
+//         character_name: req.body.character_name,
+//         class_id: req.body.class_id,
+//       }).then(
+//         req.session.save(() => {
+//           req.session.loggedIn = true;
+//         })
+//       );
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 // Login
 router.post("/login", async (req, res) => {
