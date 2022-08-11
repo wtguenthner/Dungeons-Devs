3774c14ba35cc25169703a6cb2714389c7f94baa
hackpres <102390515+hackpres@users.noreply.github.com>
2022-08-11 15:09:36 -0400

    added some battleLogic and saved various files from the damage of heroku...haha

--- public/assets/js/characterClasses.js
+++ public/assets/js/characterClasses.js
@@ -1,6 +1,6 @@
+import { probCheck, getCardAction, getCardValue } from ('../../../utils/helpers.js');
+import Card from ('./card.js');
 
-
-import probCheck from '../../../utils/helpers.js'
 class Fighter {
     constructor(name) {
         this.name = name;
@@ -11,15 +11,15 @@ class Fighter {
         this.hp -= input;
     }
 
-    attack() {
+    attack(opponent) {
         const attackProb = probCheck(50, 47);
 
         switch (attackProb) {
             case attackProb === 1:
-                opponentHash[this.name].takeDamage(this.attack)
+                opponent[this.name].takeDamage(this.attack - opponent[this.defense])
                 break;
             case attackProb === 10:
-                opponentHash[this.name].takeDamage(this.attack * 1.8)
+                opponent[this.name].takeDamage((this.attack * 1.8) - opponent[this.defense])
                 attackProb.message === 'Critical Hit!'
                 break;
         }
@@ -41,8 +41,63 @@ class Fighter {
                 break;
         }
     }
+    
+    evade() {
+        return probCheck(40, this.evasion);
+    }
 
 }
+
+const battle = async (player, opponent) => {
+    
+    //draw 3 cards
+        //card.printCard() returns [action, value] that needs to be printed on card 1
+        const card1 = new Card(getCardAction(), getCardValue())
+            //print card1.action and card1.value to DOM via handlebars
+        //card.printCard() returns [action, value] that needs to be printed on card 2
+        const card2 = new Card(getCardAction(), getCardValue())
+            //print card2.action and card2.value to DOM via handlebars
+        //card.printCard() returns [action, value] that needs to be printed on card 3
+        const card3 = new Card(getCardAction(), getCardValue())
+            //print card3.action and card3.value to DOM via handlebars
+
+    //select card to play
+        //on event "click" emphasis goes to selected card (maybe by enlarging it), and play buttons(attack, defend, evade) become visible.
+        //on different card "click" emphasis changes to newly selected card
+    //click play button
+        switch (cardAction) {
+            case 'attack':
+                player.attack += cardValue;
+                //print changed value to player card on DOM
+                break;
+            case 'defense':
+                player.defense += cardValue;
+                //print changed value to player card on DOM
+                break;
+            case 'evation':
+                player.evasion += cardValue;
+                //print changed value to player card on DOM
+                break;
+        }
+        if (e.cardAction === 'attack') {
+            player.attack(opponent);
+            bossTurn(player);
+        } else if (e.cardAction === 'defend') {
+            player.defend();
+            bossTurn(player);
+        } else if (e.cardAction === 'evade') {
+            player.evade();
+            bossTurn(player);
+        }
+        
+        
+}
+function bossTurn(player) {
+
+}
+let Kevin = new Paladin("Kevin", 8, 7, 5, 100);
+let boss = new Easy("Mob Assassin", 7, 6, 7, 100);
+battle(Kevin, boss)
 
 class Might extends Fighter {
     constructor(name) {
@@ -62,6 +117,24 @@ class Range extends Fighter {
     constructor(name) {
         super(name);
         hasProp = "Range";
+    }
+}
+
+class Boss extends Fighter {
+    constructor(name) {
+        super(name);
+        hasProp = "BOSS"
+    }
+}
+
+class Easy extends Boss {
+    constructor(name, attack, defense, evasion, hp, id) {
+        super(name);
+        this.attack = attack;
+        this.defense = defense;
+        this.evasion = evasion;
+        this.hp = hp
+        this.id = id
     }
 }
 
@@ -152,3 +225,5 @@ function characterCreate(charName, fighterChoice) {
             break;
     }
 }
+
+export { Mage, Archer, Gunslinger, Reaper, Rogue, Paladin, Easy };
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,5 @@
 
+    //probability function
 const probabilityCheck = (max, ratio1, ratio2, ratio3, ratio4, ratio5, ratio6, ratio7, ratio8, ratio9) => {
     let digit = Math.floor(Math.random() * max + 1);
   let indicator = "";
@@ -29,4 +30,33 @@ const probabilityCheck = (max, ratio1, ratio2, ratio3, ratio4, ratio5, ratio6, r
   return check(digit);
 }
 
-export default probabilityCheck;
+//get action of card
+const getCardAction = () => {
+    const actionNumber = helper.probabilityCheck(3, 1, 2);
+
+    if (actionNumber === 1) {
+        return "attack";
+    } else if (actionNumber === 2) {
+        return "defense";
+    } else if (actionNumber === 10) {
+        return "evasion";
+    }
+};
+
+//get value of card
+const getCardValue = () => {
+    return helper.probabilityCheck(100, 20, 38, 54, 68, 80, 90, 94, 97, 99);
+        // 20% of 1 
+        // 18% of 2
+        // 16% of 3
+        // 14% of 4
+        // 12% of 5
+        // 10% of 6
+        // 4% of 7
+        // 3% of 8
+        // 2% of 9
+        // 1% of 10
+    // return helper.probabilityCheck(10, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+};
+
+export { probabilityCheck, getCardAction, getCardValue };
