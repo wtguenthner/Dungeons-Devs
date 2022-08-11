33ff8816f5086c238c0812d4b689fcd1a4124da3
Taylor Guenthner <wtguenthner@gmail.com>
2022-08-11 12:31:38 -0400

    Updated api/model files

--- models/index.js
+++ models/index.js
@@ -1,4 +1,23 @@
 // const { Model } = require('sequelize/types');
-import  User   from './User.js'
+import User   from './User.js';
+import Classes from './classes.js';
+import Characters from './characters.js'
 
-export default User
+
+User.hasMany(Characters, { 
+    foreignKey: 'character_name'
+})
+
+User.hasMany(Classes, {
+    foreignKey: 'class_id'
+})
+
+Characters.belongsTo(User, {
+    foreignKey: 'character_name'
+     
+})
+
+Classes.belongsTo(User, {
+    foreignKey: 'class_id'
+})
+export {User as default, Classes, Characters}
