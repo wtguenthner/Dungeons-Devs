4e9bbbcad3b4063f021665ce3494d7c91c81a6ca
Taylor Guenthner <wtguenthner@gmail.com>
2022-08-11 20:31:31 -0400

    Fixing api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,8 @@ import express from 'express'
 const router = express.Router();
 
 
-import userRoutes  from './userRoutes.js'
+import userRoutes from './userRoutes.js'
 router.use('/users', userRoutes);
-
-
-
 
 
 export default router;
